754c6447fe54a59caea5da91f8b0761cdba66ec3
SarahAHuynh <shuynh1@outlook.com>
2024-11-14 10:33:45 -0800

    impelemented initial overlay and close/open function

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const openButton = document.getElementById("openOverlayButton");
+const closeButton = document.getElementById("closeOverlayButton");
+const closeButtonInside = document.getElementById("closeOverlayButtonInside");
+const overlay = document.getElementById("overlay");
+
+openButton.onclick = function() {
+    overlay.style.display = "block";
+}
+
+closeButton.onclick = function() {
+    overlay.style.display = "none"; 
+}
+
+closeButtonInside.onclick = function() {
+    overlay.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == overlay) {
+        overlay.style.display = "none";
+    }
+}
