b7e7168affc3105ad34c7d185d3fe55d97522d02
alison <tmrfandom@gmail.com>
2024-11-14 14:45:02 -0800

    add code skeleton for adding and removing post-its in javascript

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,20 @@
 'use strict';
+const post_it_array = []
+var last_used_id = 0
+
+// when add post-it is clicked
+function get_postit_fields(){
+    const text = ""
+    const color = ""
+    id += 1
+    return {
+        id: id,
+        text: text,
+        color: color,
+    }
+}
+
+function add_postit(id){
+    // navigate dom for the post-it with the correct id and remove it from the dom
+    // navigate post-it array for the post-it with the correct id and remove it
+}
